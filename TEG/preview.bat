33e0ae3e72c819ede5e8ae385550d858ac0dcbbe
Lewis Gaul <legaul@cisco.com>
2019-09-20 00:29:01 +0100

    Add preview batch file

---
+++ TEG/preview.bat
@@ -0,0 +1,11 @@
+@ECHO off
+CD ..
+ECHO Running...
+START /B CMD /C CALL bundler exec jekyll serve >NUL 2>&1
+TIMEOUT /t 2 >NUL 2>&1
+ECHO(
+ECHO Hello Tamsin!
+ECHO Enter 'localhost:4000' in a web browser to preview the website.
+ECHO(
+ECHO When you're finished, just close this window. Don't forget to run 'release'!
+::PAUSE
